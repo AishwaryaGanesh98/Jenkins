d5c3781ddd66c1527ef7f466296f071dcbbcd7d9
aish <aishwaryaganesh98@gmail.com>
2024-07-15 09:43:50 +0530

    first push

--- JenkinsSelenium/src/test/java/jenkins/JenkinsImplementationTest.java
+++ JenkinsSelenium/src/test/java/jenkins/JenkinsImplementationTest.java
@@ -3,7 +3,7 @@ package jenkins;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class JenkinsImplementation {
+public class JenkinsImplementationTest {
 @Test
 public void execute() {
 	Reporter.log("Jenkins class has executed", true);
