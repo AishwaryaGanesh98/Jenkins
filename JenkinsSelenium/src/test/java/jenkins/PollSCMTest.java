46f88a2ccb1b021574c4dae680a569fec125a8a9
aish <aishwaryaganesh98@gmail.com>
2024-07-16 10:03:34 +0530

    changes pushed

---
+++ JenkinsSelenium/src/test/java/jenkins/PollSCMTest.java
@@ -0,0 +1,11 @@
+package jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PollSCMTest {
+	@Test
+	public void execute() {
+		Reporter.log("PollSCMTest class is added", true);
+	}
+}
